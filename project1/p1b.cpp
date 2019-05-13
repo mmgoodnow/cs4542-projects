39297abc3c8d3e424db8d5b3468950bf9142b0c5
Michael Goodnow <mmgoodnow@gmail.com>
2019-05-13 00:00:20 -0400

    Implement p1b exhaustiveColoring and printSolution

--- project1/p1b.cpp
+++ project1/p1b.cpp
@@ -2,19 +2,16 @@
 // (BGL).
 
 #include "d_except.h"
+#include <climits>
+#include <ctime>
 #include <fstream>
 #include <iostream>
-#include <limits.h>
 #include <vector>
 
 #include <boost/graph/adjacency_list.hpp>
 
-#define LargeValue 99999999
-
 using namespace std;
 using namespace boost;
-
-int const NONE = -1; // Used to represent a node that does not exist
 
 struct VertexProperties;
 struct EdgeProperties;
@@ -22,21 +19,17 @@ struct EdgeProperties;
 typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties,
                        EdgeProperties>
     Graph;
+typedef Graph::vertex_iterator v_itr;
+typedef Graph::edge_iterator e_itr;
+typedef Graph::edge_descriptor e_dsc;
+typedef Graph::vertex_descriptor v_dsc;
 
 struct VertexProperties {
-	pair<int, int> cell; // maze cell (x,y) value
-	Graph::vertex_descriptor pred;
-	bool visited;
-	bool marked;
-	int weight;
+	int color;
 };
 
 // Create a struct to hold properties for each edge
-struct EdgeProperties {
-	int weight;
-	bool visited;
-	bool marked;
-};
+struct EdgeProperties {};
 
 // Initialize g using data from fin.
 void initializeGraph(Graph &g, ifstream &fin) {
@@ -44,11 +37,12 @@ void initializeGraph(Graph &g, ifstream &fin) {
 	int j, k;
 
 	fin >> n >> e;
-	Graph::vertex_descriptor v;
+	v_dsc v;
 
 	// Add nodes.
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) {
 		v = add_vertex(g);
+	}
 
 	for (int i = 0; i < e; i++) {
 		fin >> j >> k;
@@ -56,86 +50,86 @@ void initializeGraph(Graph &g, ifstream &fin) {
 	}
 }
 
-bool shouldIncrementAtIndex(const vector<int> &config, int numColors,
-                            int index) {
-	for (int i = 0; i < index; i++) {
-		if (config[i] != numColors - 1) return false;
+bool shouldIncrementAtIndex(Graph &g, int numColors, int index) {
+	pair<v_itr, v_itr> r = vertices(g);
+
+	// make sure that the index is within range
+	assert(r.first + index <= r.second);
+
+	for (v_itr i = r.first; i < (r.first + index); ++i) {
+		if (g[*i].color != numColors - 1) return false;
 	}
 	return true;
 }
 
-void incrementConfig(vector<int> &config, int numColors) {
-	for (int digit = config.size() - 1; digit >= 0; digit--) {
-		if (shouldIncrementAtIndex(config, numColors, digit)) {
-			config[digit]++;
-			config[digit] %= numColors;
+void incrementColoring(Graph &g, int numColors) {
+	pair<v_itr, v_itr> r = vertices(g);
+	for (v_itr i = r.second - 1; i >= r.first; --i) {
+		if (shouldIncrementAtIndex(g, numColors, i - r.first)) {
+			g[*i].color++;
+			g[*i].color %= numColors;
 		}
 	}
 }
 
-void printConfig(vector<int> &config) {
-	for (int i = 0; i < config.size(); i++) {
-		cout << config[i] << " ";
+int numConflicts(Graph &g) {
+	int conflicts = 0;
+	pair<e_itr, e_itr> r = edges(g);
+	// Loop over all edges in the graph
+	for (e_itr i = r.first; i != r.second; ++i) {
+
+		// conflicts are when neighboring nodes have the same color
+		e_dsc ed = *i;
+		v_dsc tgt = target(ed, g);
+		v_dsc src = source(ed, g);
+		if (g[tgt].color == g[src].color) conflicts++;
 	}
-	cout << endl;
+	return conflicts;
 }
 
-int numConflicts(Graph &g)
-
-int exhaustiveColoring(Graph &g, int numColors, int t) {
-	int lowestConflicts = LargeValue
+int exhaustiveColoring(Graph &g, int numColors, int secs) {
+	int lowestConflicts = INT_MAX;
+	Graph lowestConflictsGraph;
 	clock_t startTime = clock();
 	while (true) {
 
-		// check validity and compare value against current best
-		if (k.getCost() <= k.getCostLimit() && k.getValue() > bestValue) {
-			bestValue = k.getValue();
-			for (int j = 0; j < k.getNumObjects(); j++) {
-				currentBest[j] = k.isSelected(j);
-			}
-		}
-
-		// iterate currentConfig by walking down the bits backwards to
-		// determine if they should be flipped
-		for (int bit = k.getNumObjects() - 1; bit >= 0; bit--) {
-			if (shouldFlipBit(currentConfig, bit)) {
-				currentConfig[bit] = !currentConfig[bit];
-			}
-		}
-
-		// set k to currentConfig
-		for (int bit = 0; bit < k.getNumObjects(); bit++) {
-			if (currentConfig[bit]) {
-				k.select(bit);
-			} else {
-				k.unSelect(bit);
-			}
+		// compare value against current best
+		int conflicts = numConflicts(g);
+		if (numConflicts(g) < lowestConflicts) {
+			lowestConflicts = conflicts;
+			cerr << lowestConflicts << endl;
+			lowestConflictsGraph = g;
+			assert(numConflicts(lowestConflictsGraph) == numConflicts(g));
+		} else {
+			assert(numConflicts(lowestConflictsGraph) <= numConflicts(g));
 		}
 
 		// terminate if we're on the last configuration
-		if (shouldIncrementAtIndex(config, numColors, config.size())) break;
+		if (shouldIncrementAtIndex(g, numColors, num_vertices(g))) break;
 
 		// terminate if it's been running too long
 		if ((clock() - startTime) / CLOCKS_PER_SEC >= secs) break;
+
+		incrementColoring(g, numColors);
 	}
+	g = lowestConflictsGraph;
+	return lowestConflicts;
 }
 
-// Set all node weights to w.
-void setNodeWeights(Graph &g, int w) {
-	pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange =
-	    vertices(g);
+void printSolution(Graph &g) {
+	cout << "------------------------------------------------" << endl;
+	cout << "Num conflicts: " << numConflicts(g) << endl;
+	pair<v_itr, v_itr> r = vertices(g);
 
-	for (Graph::vertex_iterator vItr = vItrRange.first;
-	     vItr != vItrRange.second; ++vItr) {
-		g[*vItr].weight = w;
+	for (v_itr i = r.first; i < r.second; ++i) {
+		cout << *i << " " << g[*i].color << endl;
 	}
 }
 
 int main(int argc, char *argv[]) {
-	char x;
 	ifstream fin;
 	if (argc != 2) {
-		cout << "Usage:\n\t./p1a input_file" << endl;
+		cerr << "Usage:\n\t./p1a input_file" << endl;
 		exit(1);
 	}
 	fin.open(argv[1]);
@@ -145,15 +139,27 @@ int main(int argc, char *argv[]) {
 	}
 
 	try {
-		cout << "Reading graph" << endl;
+		cerr << "Reading graph" << endl;
 		Graph g;
+		int numColors;
+		fin >> numColors;
 		initializeGraph(g, fin);
 
+		cout << "Num colors: " << numColors << endl;
 		cout << "Num nodes: " << num_vertices(g) << endl;
 		cout << "Num edges: " << num_edges(g) << endl;
 		cout << endl;
 
-		// cout << g;
+		exhaustiveColoring(g, numColors, 600);
+
+		printSolution(g);
+
+	} catch (indexRangeError &ex) {
+		cerr << ex.what() << endl;
+		exit(1);
+	} catch (rangeError &ex) {
+		cerr << ex.what() << endl;
+		exit(1);
 	}
 	return 0;
 }
