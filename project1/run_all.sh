939f6e4d8972ec04e72d54bbc4fc2aee91be9aac
Michael Goodnow <mmgoodnow@gmail.com>
2019-05-13 02:14:58 -0400

    Modify run_all to run in parallel

--- project1/run_all.sh
+++ project1/run_all.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 make
+N=4
+(
 for if in knapsack/*.input; do
+	((i=i%N)); ((i++==0)) && wait
     of=${if/input/output}
-    ./p1a $if > $of
+    echo "./p1a $if > $of"
+    ./p1a $if > $of &
 done
 for if in color/*.input; do
+	((i=i%N)); ((i++==0)) && wait
     of=${if/input/output}
-    ./p1b $if > $of
+    echo "./p1b $if > $of"
+    ./p1b $if > $of &
 done
+)
+wait
 make clean
